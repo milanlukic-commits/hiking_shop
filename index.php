4c139d27efdd25fe1a06eaf5d709087c4f9632c1
milanlukic_commitse <milanlukic.se@gmail.com>
2021-09-04 15:30:08 +0200

    starter

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="../hiking_shop/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../hiking_shop/css/owl.theme.default.min.css">
+
+    <title>Hiking shop</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+    <!-- topbar -->
+    <section class="topbar">
+        <h1>
+            <a class="logo" href="index.html"><i class="fas fa-hiking"></i>Hiking shop</a>
+        </h1>
+    </section>
+
+    <!-- nav -->
+    <nav>
+        <ul>
+            <li>
+                <a class="active" href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="products.html">Products</a>
+            </li>
+            <li>
+                <a href="about_us.html">About us</a>
+            </li>
+            <li>
+                <a href="contact_us.html">Contact us</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- header -->
+    <header>
+        <article>
+            <h1>Online outdoor shop, buy online outdoor clothing & trekking</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quos pariatur architecto hic ex exercitationem veniam, non, ullam excepturi, fuga reprehenderit laudantium consequuntur veritatis alias voluptate inventore placeat officia minima?</p>
+            <button>Order now</button>
+        </article>
+    </header>
+
+    <!-- featuredItems -->
+    <section class="featuredItems container">
+        <h2>Featured items</h2>
+        <article class="owl-carousel owl-theme">
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/boots.jpg" alt="">
+                    <h3>Boots</h3>
+                    <p>$220</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/gloves.jpg" alt="">
+                    <h3>GLoves</h3>
+                    <p>$20</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/jackets.jpg" alt="">
+                    <h3>Jackets</h3>
+                    <p>$290</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/pants.jpg" alt="">
+                    <h3>Pants</h3>
+                    <p>$110</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/poles.jpg" alt="">
+                    <h3>Poles</h3>
+                    <p>$50</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img//shirts.jpg" alt="">
+                    <h3>Shirts</h3>
+                    <p>$80</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/skirts.jpg" alt="">
+                    <h3>Skirts</h3>
+                    <p>$90</p>
+                </a>
+            </div>
+            <div class="item">
+                <a href="single.html">
+                    <img src="img/socks.jpg" alt="">
+                    <h3>Socks</h3>
+                    <p>$10</p>
+                </a>
+            </div>
+        </article>
+    </section>
+
+    <!-- subscribe -->
+    <section class="subscribe">
+        <article class="container">
+            <h3>Subscribe on Hiking shop now!</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br> Fuga accusantium labore voluptatum laudantium id, rem optio nemo minus voluptatibus voluptas.</p>
+            <form>
+                <input type="text" placeholder="Your email...">
+                <input type="submit" value="Subscribe">
+            </form>
+        </article>
+    </section>
+
+    <!-- footer -->
+    <footer>
+        <article  class="container">
+        <h3>
+            <a class="logo" href="index.html"><i class="fas fa-hiking"></i>Hiking shop</a>
+        </h3>
+        <nav>
+            <ul>
+                <li>
+                    <a class="active" href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="products.html">Products</a>
+                </li>
+                <li>
+                    <a href="about_us.html">About us</a>
+                </li>
+                <li>
+                    <a href="contact_us.html">Contact us</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="icons">
+            <span><a href=""><i class="fab fa-facebook-square"></i></a></span>
+            <span><a href=""><i class="fab fa-instagram"></i></a></span>
+            <span><a href=""><i class="fab fa-twitter"></i></a></span>
+            <span><a href=""><i class="fab fa-pinterest"></i></a></span>
+        </div>
+        </article>
+        <article>
+            <p>&copy; Hiking shop 2021.</p>
+        </article>
+    </footer>
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="../hikingshop/js/owl.carousel.min.js"></script>
+    <script src="../hikingshop/js/main.js"></script>
+
+
+</body>
+</html>
